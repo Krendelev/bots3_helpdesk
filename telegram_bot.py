78677c7594cf67b17ce6fdd44647b71b8732f74a
GK-dvmn <krendelev@gmail.com>
2021-07-15 03:43:54 +0300

    update logging

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,12 @@
 import logging
 import os
+from logging.handlers import RotatingFileHandler
 
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 from utils import reply_with_intent
+
+logger = logging.getLogger(__file__)
 
 
 def start(update, context):
@@ -11,17 +14,18 @@ def start(update, context):
 
 
 def reply(update, context):
-    message = reply_with_intent(update.effective_chat.id, update.message.text)
+    message, _ = reply_with_intent(update.effective_chat.id, update.message.text)
     update.message.reply_text(message)
 
 
-if __name__ == "__main__":
+def main():
     logging.basicConfig(
-        filename="bot.log",
-        filemode="w",
+        filename="tg_bot.log",
         format="%(asctime)s - %(levelname)s - %(message)s",
-        level=logging.INFO,
+        level=logging.WARNING,
     )
+    handler = RotatingFileHandler("tg_bot.log", maxBytes=200, backupCount=2)
+    logger.addHandler(handler)
 
     bot = Updater(os.environ["TELEGRAM_TOKEN"])
 
@@ -30,3 +34,7 @@ if __name__ == "__main__":
 
     bot.start_polling()
     bot.idle()
+
+
+if __name__ == "__main__":
+    main()
